7a6a45a4fc6a2d0e50ed92516f9989cb6afab301
shouta0715 <Myheroes0715@gmail.com>
2023-09-07 20:51:10 +0900

    add pages

---
+++ src/app/about/page.tsx
@@ -0,0 +1,10 @@
+import { Metadata } from "next";
+import React from "react";
+
+export const metadata: Metadata = {
+  title: "about",
+};
+
+export default function Page() {
+  return <div>page</div>;
+}
---
+++ src/app/works/page.tsx
@@ -0,0 +1,10 @@
+import { Metadata } from "next";
+import React from "react";
+
+export const metadata: Metadata = {
+  title: "works",
+};
+
+export default function Page() {
+  return <div>page</div>;
+}
