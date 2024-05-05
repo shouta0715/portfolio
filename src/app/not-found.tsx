cf9d226cc41abe72c6b67a9225bdfee41e776926
shouta0715 <Myheroes0715@gmail.com>
2024-05-05 15:17:59 +0900

    fix: not found

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -12,10 +12,10 @@ export default function NotFound() {
             <p className="text-base font-semibold leading-8 text-destructive">
               404
             </p>
-            <h1 className="mt-4 text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl">
+            <h1 className="mt-4 text-3xl font-bold tracking-tight text-primary sm:text-5xl">
               Page not found
             </h1>
-            <p className="mt-6 text-base leading-7 text-gray-600">
+            <p className="mt-6 text-base leading-7 text-muted-foreground">
               お探しのページが見つかりませんでした。
             </p>
             <div className="mt-10">
